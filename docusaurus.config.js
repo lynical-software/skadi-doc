4cdafaddcc8f74fdf4a5d473e5cda124415fb017
Thong Chunlee <thong.chunlee@ababank.com>
2022-09-20 12:25:01 +0700

    update doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Lynical', // Usually your GitHub org/user name.
+  organizationName: 'lynical-software', // Usually your GitHub org/user name.
   projectName: 'skadi-doc', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
